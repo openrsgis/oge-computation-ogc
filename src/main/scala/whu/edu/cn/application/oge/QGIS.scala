ff9564c0089fb40e8a3ba381abad139b1a807bf9
RuixiangLiuWHU <lrx_lucky@whu.edu.cn>
2023-05-29 19:56:55 +0800

    新增了统一类

--- src/main/scala/whu/edu/cn/application/oge/QGIS.scala
+++ src/main/scala/whu/edu/cn/application/oge/QGIS.scala
@@ -13,6 +13,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.locationtech.jts.geom._
 import whu.edu.cn.core.entity.SpaceTimeBandKey
+import whu.edu.cn.util.RDDTransformerUtil._
 import whu.edu.cn.util.SSHClientUtil._
 import whu.edu.cn.util.ShapeUtil
 import whu.edu.cn.util.ShapeUtil.readShp
@@ -29,16 +30,10 @@ object QGIS {
   //输入输出都是TIF的
   def aspect(implicit sc: SparkContext, input: (RDD[(SpaceTimeBandKey, Tile)], TileLayerMetadata[SpaceTimeKey]), zFactor: Float): (RDD[(SpaceTimeBandKey, Tile)], TileLayerMetadata[SpaceTimeKey]) = {
     //RDD转换为TIF，QGIS可以直接使用
-    val tileLayerArray = input._1.map(t => {
-      (t._1.spaceTimeKey.spatialKey, t._2)
-    }).collect()
-    val layout = input._2.layout
-    val (tile, (_, _), (_, _)) = TileLayoutStitcher.stitch(tileLayerArray)
-    val stitchedTile = Raster(tile, layout.extent)
     val time = System.currentTimeMillis()
     val outputTiffPath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/aspect_" + time + ".tif"
-    GeoTiff(stitchedTile, input._2.crs).write(outputTiffPath)
     val writePath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/aspect_" + time + "_out.tif"
+    saveRasterRDDToTif(input, outputTiffPath)
 
     //如果不符合参数要求，抛出RuntimeException异常
     //throw RuntimeException("Args do not follow the rules.")
@@ -66,36 +61,15 @@ object QGIS {
     }
 
     //QGIS生成了一个TIF, 如果前后的分辨率、投影坐标系、范围都没变，可以直接复用这个代码
-    val hadoopPath = "file://" + writePath
-    val inputRdd: RDD[(ProjectedExtent, Tile)] = HadoopGeoTiffRDD.spatial(new Path(hadoopPath))(sc)
-    val tiled = inputRdd.tileToLayout(DoubleCellType, layout, Bilinear)
-    val srcLayout = input._2.layout
-    val srcExtent = input._2.extent
-    val srcCrs = input._2.crs
-    val srcBounds = input._2.bounds
-    val now = "1000-01-01 00:00:00"
-    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    val date = sdf.parse(now).getTime
-    val newBounds = Bounds(SpaceTimeKey(srcBounds.get.minKey.spatialKey._1, srcBounds.get.minKey.spatialKey._2, date), SpaceTimeKey(srcBounds.get.maxKey.spatialKey._1, srcBounds.get.maxKey.spatialKey._2, date))
-    val metaData = TileLayerMetadata(DoubleCellType, srcLayout, srcExtent, srcCrs, newBounds)
-    val tiledOut = tiled.map(t => {
-      (SpaceTimeBandKey(SpaceTimeKey(t._1._1, t._1._2, date), "Aspect"), t._2)
-    })
-    (tiledOut, metaData)
+    makeRasterRDDFromTif(sc, input, writePath)
   }
 
   def aspect2(implicit sc: SparkContext, input: (RDD[(SpaceTimeBandKey, Tile)], TileLayerMetadata[SpaceTimeKey]), zFactor: Float): (RDD[(SpaceTimeBandKey, Tile)], TileLayerMetadata[SpaceTimeKey]) = {
     //RDD转换为TIF，QGIS可以直接使用
-    val tileLayerArray = input._1.map(t => {
-      (t._1.spaceTimeKey.spatialKey, t._2)
-    }).collect()
-    val layout = input._2.layout
-    val (tile, (_, _), (_, _)) = TileLayoutStitcher.stitch(tileLayerArray)
-    val stitchedTile = Raster(tile, layout.extent)
     val time = System.currentTimeMillis()
     val outputTiffPath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/aspect_" + time + ".tif"
-    GeoTiff(stitchedTile, input._2.crs).write(outputTiffPath)
     val writePath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/aspect_" + time + "_out.tif"
+    saveRasterRDDToTif(input, outputTiffPath)
 
     //如果不符合参数要求，抛出RuntimeException异常
     //throw RuntimeException("Arg does not follow the rule.")
@@ -123,35 +97,14 @@ object QGIS {
     }
 
     //如果变了
-    val hadoopPath = "file://" + writePath
-    val inputRdd: RDD[(ProjectedExtent, Tile)] = HadoopGeoTiffRDD.spatial(new Path(hadoopPath))(sc)
-    val localLayoutScheme = FloatingLayoutScheme(256)
-    val (_: Int, metadata: TileLayerMetadata[SpatialKey]) =
-      inputRdd.collectMetadata[SpatialKey](localLayoutScheme)
-    val tiled = inputRdd.tileToLayout[SpatialKey](metadata).cache()
-    val srcLayout = metadata.layout
-    val srcExtent = metadata.extent
-    val srcCrs = metadata.crs
-    val srcBounds = metadata.bounds
-    val now = "1000-01-01 00:00:00"
-    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    val date = sdf.parse(now).getTime
-    val newBounds = Bounds(SpaceTimeKey(srcBounds.get.minKey._1, srcBounds.get.minKey._2, date), SpaceTimeKey(srcBounds.get.maxKey._1, srcBounds.get.maxKey._2, date))
-    val metaData = TileLayerMetadata(DoubleCellType, srcLayout, srcExtent, srcCrs, newBounds)
-    val tiledOut = tiled.map(t => {
-      (SpaceTimeBandKey(SpaceTimeKey(t._1._1, t._1._2, date), "Aspect"), t._2)
-    })
-    (tiledOut, metaData)
+    makeChangedRasterRDDFromTif(sc, writePath)
   }
 
   def randomPointsAlongLine(implicit sc: SparkContext, input: RDD[(String, (Geometry, Map[String, Any]))], pointsNumber: Int = 1, minDistance: Double = 0.0): RDD[(String, (Geometry, Map[String, Any]))] = {
-    val data = input.map(t=>{
-      t._2._2 + (ShapeUtil.DEF_GEOM_KEY -> t._2._1)
-    }).collect().map(_.asJava).toList.asJava
     val time = System.currentTimeMillis()
     val outputTiffPath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/randomPointsAlongLine_" + time + ".shp"
-    ShapeUtil.createShp(outputTiffPath, "utf-8", classOf[LineString], data)
     val writePath = "/home/geocube/oge/oge-server/dag-boot/qgis/algorithmData/aspect_" + time + "_out.shp"
+    saveFeatureRDDToShp(input, outputTiffPath)
 
     //如果不符合参数要求，抛出RuntimeException异常
     //throw RuntimeException("Arg does not follow the rule.")
@@ -179,6 +132,7 @@ object QGIS {
       case e: Exception =>
         e.printStackTrace()
     }
-    readShp(sc, writePath, "utf-8")
+
+    makeFeatureRDDFromShp(sc, writePath)
   }
 }
