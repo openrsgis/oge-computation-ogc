43af81ef68600418883f3f22989e72b20c193ebd
Wang <2018302130234@whu.edu.cn>
2023-09-10 11:17:57 +0800

    feat:增加Cube的案例实现一个简单的demo

--- src/main/scala/whu/edu/cn/geocube/core/entity/QueryParams.scala
+++ src/main/scala/whu/edu/cn/geocube/core/entity/QueryParams.scala
@@ -1,5 +1,7 @@
 package whu.edu.cn.geocube.core.entity
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
 import java.sql.{DriverManager, ResultSet}
 import geotrellis.layer.LayoutDefinition
 import geotrellis.raster.TileLayout
@@ -7,9 +9,10 @@ import org.locationtech.jts.geom.{Coordinate, GeometryFactory, Polygon}
 
 import scala.collection.mutable.ArrayBuffer
 import whu.edu.cn.geocube.core.vector.grid.GridTransformer.getGeomZcodes
-import whu.edu.cn.util.GlobalConstantUtil.{POSTGRESQL_PWD, POSTGRESQL_URL, POSTGRESQL_USER}
 import whu.edu.cn.util.PostgresqlUtil
 
+import scala.beans.BeanProperty
+import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.collection.mutable
 
 /**
@@ -17,99 +20,175 @@ import scala.collection.mutable
  *
  */
 class QueryParams extends Serializable {
-  var cubeId:String = ""
+  @JsonSerialize
+  var cubeId: String = ""
+  @JsonSerialize
   var rasterProductName: String = ""
+  @JsonSerialize
   var rasterProductNames: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var vectorProductName: String = ""
+  @JsonSerialize
   var vectorProductNames: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var tabularProductName: String = ""
+  @JsonSerialize
   var platform: String = ""
+  @JsonSerialize
   var instruments: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var measurements: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var highLevelMeasurement: String = ""
+  @JsonSerialize
   var CRS: String = ""
+  @JsonSerialize
   var tileSize: String = ""
+  @JsonSerialize
   var cellRes: String = ""
-  //  var level: String = "4000"
+  @JsonSerialize
   var level: String = ""
+  @JsonSerialize
   var phenomenonTime: String = ""
+  @JsonSerialize
   var startTime: String = ""
+  @JsonSerialize
   var endTime: String = ""
+  @JsonSerialize
   var nextStartTime: String = ""
+  @JsonSerialize
   var nextEndTime: String = ""
+  @JsonSerialize
   var month: Int = -1
+  @JsonSerialize
   var year: Int = -1
+  @JsonSerialize
   var gridCodes: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var cityCodes: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var cityNames: ArrayBuffer[String] = new ArrayBuffer[String]()
+  @JsonSerialize
   var cityName: String = ""
+  @JsonSerialize
   var provinceName: String = ""
+  @JsonSerialize
   var districtName: String = ""
+  @JsonSerialize
   var cloudMax: String = ""
+  @JsonSerialize
   var cloudShadowMax: String = ""
+  @JsonSerialize
   var polygon: Polygon = null
+  @JsonSerialize
+  var extentCoordinates: ArrayBuffer[Double] = new ArrayBuffer[Double]()
+  @JsonSerialize
+  var subsetQuery: ArrayBuffer[SubsetQuery] = new ArrayBuffer[SubsetQuery]()
+  //  var subsetQuery: java.util.List[SubsetQuery] = new java.util.ArrayList[SubsetQuery]()
 
-  def this(platform: String, measurements: ArrayBuffer[String],
-           gridCodes: ArrayBuffer[String], cloud: String, level: String) {
+  def this(cubeId: String, rasterProductName: String, rasterProductNames: ArrayBuffer[String], vectorProductName: String,
+           vectorProductNames: ArrayBuffer[String], tabularProductName: String, platform: String, instruments: ArrayBuffer[String],
+           measurements: ArrayBuffer[String], highLevelMeasurement: String, CRS: String, tileSize: String, cellRes: String,
+           level: String, phenomenonTime: String, startTime: String, endTime: String, nextStartTime: String, nextEndTime: String,
+           month: Int, year: Int, gridCodes: ArrayBuffer[String], cityCodes: ArrayBuffer[String], cityNames: ArrayBuffer[String],
+           cityName: String, provinceName: String, districtName: String, cloudMax: String, cloudShadowMax: String, polygon: Polygon,
+           extentCoordinates: ArrayBuffer[Double], subsetQuery: ArrayBuffer[SubsetQuery]) {
     this()
-    this.platform = platform
-    this.measurements = measurements
-    this.gridCodes = gridCodes
-    this.cloudMax = cloud
-    this.level = level
-  }
-
-  def this(rasterProductName: String, platform: String, instruments: ArrayBuffer[String],
-           measurements: ArrayBuffer[String], CRS: String, tileSize: String,
-           cellRes: String, level: String, phenomenonTime: String,
-           startTime: String, endTime: String, gridCodes: ArrayBuffer[String],
-           cityCodes: ArrayBuffer[String], cityNames: ArrayBuffer[String], cloud: String, cloudShadow: String) {
-    this()
+    this.cubeId = cubeId
     this.rasterProductName = rasterProductName
+    this.rasterProductNames = rasterProductNames
+    this.vectorProductName = vectorProductName
+    this.vectorProductNames = vectorProductNames
+    this.tabularProductName = tabularProductName
     this.platform = platform
     this.instruments = instruments
     this.measurements = measurements
+    this.highLevelMeasurement = highLevelMeasurement
     this.CRS = CRS
     this.tileSize = tileSize
     this.cellRes = cellRes
+    this.level = level
+    this.phenomenonTime = phenomenonTime
     this.startTime = startTime
     this.endTime = endTime
     this.nextStartTime = nextStartTime
     this.nextEndTime = nextEndTime
-    this.phenomenonTime = phenomenonTime
+    this.month = month
+    this.year = year
     this.gridCodes = gridCodes
     this.cityCodes = cityCodes
     this.cityNames = cityNames
-    this.cloudMax = cloud
-    this.cloudShadowMax = cloudShadow
-    this.level = level
+    this.cityName = cityName
+    this.provinceName = provinceName
+    this.districtName = districtName
+    this.cloudMax = cloudMax
+    this.cloudShadowMax = cloudShadowMax
+    this.polygon = polygon
+    this.extentCoordinates = extentCoordinates
+    this.subsetQuery = subsetQuery
   }
 
-  def this(rasterProductNames: ArrayBuffer[String], platform: String, instruments: ArrayBuffer[String],
-           measurements: ArrayBuffer[String], CRS: String, tileSize: String,
-           cellRes: String, level: String, phenomenonTime: String,
-           startTime: String, endTime: String, gridCodes: ArrayBuffer[String],
-           cityCodes: ArrayBuffer[String], cityNames: ArrayBuffer[String], cloud: String, cloudShadow: String) {
-    this()
-    this.rasterProductNames = rasterProductNames
-    this.platform = platform
-    this.instruments = instruments
-    this.measurements = measurements
-    this.CRS = CRS
-    this.tileSize = tileSize
-    this.cellRes = cellRes
-    this.startTime = startTime
-    this.endTime = endTime
-    this.nextStartTime = nextStartTime
-    this.nextEndTime = nextEndTime
-    this.phenomenonTime = phenomenonTime
-    this.gridCodes = gridCodes
-    this.cityCodes = cityCodes
-    this.cityNames = cityNames
-    this.cloudMax = cloud
-    this.cloudShadowMax = cloudShadow
-    this.level = level
-  }
+  //  def this(platform: String, measurements: ArrayBuffer[String],
+  //           gridCodes: ArrayBuffer[String], cloud: String, level: String) {
+  //    this()
+  //    this.platform = platform
+  //    this.measurements = measurements
+  //    this.gridCodes = gridCodes
+  //    this.cloudMax = cloud
+  //    this.level = level
+  //  }
+  //
+  //  def this(rasterProductName: String, platform: String, instruments: ArrayBuffer[String],
+  //           measurements: ArrayBuffer[String], CRS: String, tileSize: String,
+  //           cellRes: String, level: String, phenomenonTime: String,
+  //           startTime: String, endTime: String, gridCodes: ArrayBuffer[String],
+  //           cityCodes: ArrayBuffer[String], cityNames: ArrayBuffer[String], cloud: String, cloudShadow: String) {
+  //    this()
+  //    this.rasterProductName = rasterProductName
+  //    this.platform = platform
+  //    this.instruments = instruments
+  //    this.measurements = measurements
+  //    this.CRS = CRS
+  //    this.tileSize = tileSize
+  //    this.cellRes = cellRes
+  //    this.startTime = startTime
+  //    this.endTime = endTime
+  //    this.nextStartTime = nextStartTime
+  //    this.nextEndTime = nextEndTime
+  //    this.phenomenonTime = phenomenonTime
+  //    this.gridCodes = gridCodes
+  //    this.cityCodes = cityCodes
+  //    this.cityNames = cityNames
+  //    this.cloudMax = cloud
+  //    this.cloudShadowMax = cloudShadow
+  //    this.level = level
+  //  }
+  //
+  //  def this(rasterProductNames: ArrayBuffer[String], platform: String, instruments: ArrayBuffer[String],
+  //           measurements: ArrayBuffer[String], CRS: String, tileSize: String,
+  //           cellRes: String, level: String, phenomenonTime: String,
+  //           startTime: String, endTime: String, gridCodes: ArrayBuffer[String],
+  //           cityCodes: ArrayBuffer[String], cityNames: ArrayBuffer[String], cloud: String, cloudShadow: String) {
+  //    this()
+  //    this.rasterProductNames = rasterProductNames
+  //    this.platform = platform
+  //    this.instruments = instruments
+  //    this.measurements = measurements
+  //    this.CRS = CRS
+  //    this.tileSize = tileSize
+  //    this.cellRes = cellRes
+  //    this.startTime = startTime
+  //    this.endTime = endTime
+  //    this.nextStartTime = nextStartTime
+  //    this.nextEndTime = nextEndTime
+  //    this.phenomenonTime = phenomenonTime
+  //    this.gridCodes = gridCodes
+  //    this.cityCodes = cityCodes
+  //    this.cityNames = cityNames
+  //    this.cloudMax = cloud
+  //    this.cloudShadowMax = cloudShadow
+  //    this.level = level
+  //  }
 
   /**
    * Set grid codes with input extent.
@@ -119,28 +198,46 @@ class QueryParams extends Serializable {
    * @param rightUpperLong
    * @param rightUpperLat
    */
-  def setExtent(leftBottomLong: Double, LeftBottomLat: Double, rightUpperLong: Double, rightUpperLat: Double): Unit = {
+  def setExtent(leftBottomLong: Double, leftBottomLat: Double, rightUpperLong: Double, rightUpperLat: Double): Unit = {
+    this.extentCoordinates.append(leftBottomLong, leftBottomLat, rightUpperLong, rightUpperLat)
     val queryGeometry = new GeometryFactory().createPolygon(Array(
-      new Coordinate(leftBottomLong, LeftBottomLat),
+      new Coordinate(leftBottomLong, leftBottomLat),
       new Coordinate(leftBottomLong, rightUpperLat),
       new Coordinate(rightUpperLong, rightUpperLat),
-      new Coordinate(rightUpperLong, LeftBottomLat),
-      new Coordinate(leftBottomLong, LeftBottomLat)
+      new Coordinate(rightUpperLong, leftBottomLat),
+      new Coordinate(leftBottomLong, leftBottomLat)
     ))
     this.polygon = queryGeometry
     val id = this.getCubeId
-    val SRE = getSizeResAndExtentByCubeId(id,POSTGRESQL_URL, POSTGRESQL_USER, POSTGRESQL_PWD).toList.head
-    //    val extent = geotrellis.vector.Extent(-180, -90, 180, 90)
-    //    val extent = geotrellis.vector.Extent(113, 23, 114, 24)
+    PostgresqlUtil.get()
+    val SRE = getSizeResAndExtentByCubeId(id, PostgresqlUtil.url, PostgresqlUtil.user, PostgresqlUtil.password).toList.head
     val extent = geotrellis.vector.Extent(SRE._2._1, SRE._2._2, SRE._2._3, SRE._2._4)
-    //    val tl = TileLayout(360, 180, 1024, 1024) //1024 can be replaced by any value
     println(SRE)
-    //    val extent = geotrellis.vector.Extent(113, 23, 114, 24)
-    val tl = TileLayout(((SRE._2._3 - SRE._2._1) / SRE._1._1).toInt, ((SRE._2._4 - SRE._2._2) / SRE._1._1).toInt, SRE._1._2.toInt,  SRE._1._2.toInt) //1024 can be replaced by any value
+    val tl = TileLayout(((SRE._2._3 - SRE._2._1) / SRE._1._1).toInt, ((SRE._2._4 - SRE._2._2) / SRE._1._1).toInt, SRE._1._2.toInt, SRE._1._2.toInt) //1024 can be replaced by any value
     val ld = LayoutDefinition(extent, tl)
-    //    this.setLevel(SRE._1._2.toInt.toString)
     val gridCodeArray = getGeomZcodes(queryGeometry, ld.layoutCols, ld.layoutRows, ld.extent, SRE._1._1)
     this.setGridCodes(gridCodeArray)
+  }
+
+    def setPolygon(polygon: Polygon): Unit = {
+      this.polygon = polygon
+    }
+
+  def updatePolygon(): Unit = {
+    if (!this.extentCoordinates.isEmpty) {
+      val leftBottomLong = this.extentCoordinates(0)
+      val leftBottomLat = this.extentCoordinates(1)
+      val rightUpperLong = this.extentCoordinates(2)
+      val rightUpperLat = this.extentCoordinates(3)
+      val queryGeometry = new GeometryFactory().createPolygon(Array(
+        new Coordinate(leftBottomLong, leftBottomLat),
+        new Coordinate(leftBottomLong, rightUpperLat),
+        new Coordinate(rightUpperLong, rightUpperLat),
+        new Coordinate(rightUpperLong, leftBottomLat),
+        new Coordinate(leftBottomLong, leftBottomLat)
+      ))
+      this.polygon = queryGeometry
+    }
   }
 
   /**
@@ -157,6 +254,7 @@ class QueryParams extends Serializable {
       case _ => throw new RuntimeException("No support for " + districtType)
     }
   }
+
 
   /**
    * Set query time range.
@@ -180,6 +278,7 @@ class QueryParams extends Serializable {
     this.setTime(previousStartTime, previousEndTime)
   }
 
+
   /**
    * Set next time range, which is used in two-period analysis,
    * e.g. water change detection.
@@ -191,6 +290,7 @@ class QueryParams extends Serializable {
     this.setNextStartTime(nextStartTime)
     this.setNextEndTime(nextEndTime)
   }
+
 
   def setMonth(month: Int): Unit = {
     this.month = month
@@ -212,6 +312,7 @@ class QueryParams extends Serializable {
     this.rasterProductNames = rasterProductNames.toBuffer.asInstanceOf[ArrayBuffer[String]]
   }
 
+
   def setVectorProductName(vectorProductName: String): Unit = {
     this.vectorProductName = vectorProductName
   }
@@ -223,6 +324,7 @@ class QueryParams extends Serializable {
   def setVectorProductNames(vectorProductNames: Array[String]): Unit = {
     this.vectorProductNames = vectorProductNames.toBuffer.asInstanceOf[ArrayBuffer[String]]
   }
+
 
   def setTabularProductName(tabularProductName: String): Unit = {
     this.tabularProductName = tabularProductName
@@ -237,9 +339,7 @@ class QueryParams extends Serializable {
   }
 
   def setMeasurements(measurements: Array[String]): Unit = {
-    if (measurements != null) {
-      this.measurements = measurements.toBuffer.asInstanceOf[ArrayBuffer[String]]
-    }
+    this.measurements = measurements.toBuffer.asInstanceOf[ArrayBuffer[String]]
   }
 
   def setMeasurements(measurements: ArrayBuffer[String]): Unit = {
@@ -317,75 +417,108 @@ class QueryParams extends Serializable {
   def setTileSize(tileSize: String): Unit = {
     this.tileSize = tileSize
   }
+
   def setCubeId(cubeId: String): Unit = {
     this.cubeId = cubeId
   }
 
+  def setExtentCoordinates(extentCoordinates: ArrayBuffer[Double]): Unit = {
+    this.extentCoordinates = extentCoordinates
+  }
+
+  def setSubsetQuery(subsetQuery: ArrayBuffer[SubsetQuery]): Unit = {
+    this.subsetQuery = subsetQuery
+  }
+
+  def setSubsetQuery(subsetQuery: java.util.List[SubsetQuery]): Unit = {
+    this.subsetQuery = ArrayBuffer(subsetQuery.asScala: _*)
+  }
+
   def getPolygon: Polygon = polygon
+
   def getPlatform: String = platform
+
   def getPhenomenonTime: String = phenomenonTime
+
   def getRasterProductName: String = rasterProductName
+
   def getRasterProductNames: ArrayBuffer[String] = rasterProductNames
+
   def getVectorProductName: String = vectorProductName
+
   def getVectorProductNames: ArrayBuffer[String] = vectorProductNames
+
   def getTabularProductName: String = tabularProductName
+
   def getStartTime: String = startTime
+
   def getEndTime: String = endTime
+
   def getNextStartTime: String = nextStartTime
+
   def getNextEndTime: String = nextEndTime
+
   def getMonth: Int = month
+
   def getYear: Int = year
+
   def getLevel: String = level
+
   def getTileSize: String = tileSize
+
   def getGridCodes: ArrayBuffer[String] = gridCodes
+
   def getCloudMax: String = cloudMax
+
   def getCloudShadowMax: String = cloudShadowMax
+
   def getCRS: String = CRS
+
   def getCellRes: String = cellRes
+
   def getMeasurements: ArrayBuffer[String] = measurements
+
   def getHighLevelMeasurement: String = highLevelMeasurement
+
   def getCityNames: ArrayBuffer[String] = cityNames
+
   def getCityCodes: ArrayBuffer[String] = cityCodes
+
   def getInstruments: ArrayBuffer[String] = instruments
+
   def getDistrictName: String = districtName
+
   def getProvinceName: String = provinceName
-  def getCityName:String = cityName
-  def getCubeId:String = cubeId
+
+  def getCityName: String = cityName
+
+  def getCubeId: String = cubeId
+
+  def getExtentCoordinates: ArrayBuffer[Double] = extentCoordinates
+
+  def getSubsetQuery: ArrayBuffer[SubsetQuery] = this.subsetQuery
 
   /**
    * Get cell size and resolution info from SensorLevelAndProduct view, and return a HashMap object.
+   *
    * @param cubeId
    * @param connAddr
    * @param user
    * @param password
-   *
    * @return a HashMap object.
    */
-  def getSizeResAndExtentByCubeId(cubeId:String,  connAddr: String, user: String, password: String): mutable.HashMap[(Double,Double),(Double,Double,Double,Double)] = {
+  def getSizeResAndExtentByCubeId(cubeId: String, connAddr: String, user: String, password: String): mutable.HashMap[(Double, Double), (Double, Double, Double, Double)] = {
     val conn = DriverManager.getConnection(connAddr, user, password)
     if (conn != null) {
       try {
         val statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
         val sql = "select distinct cell_size,cell_res,slice_minx,slice_miny,slice_maxx,slice_maxy " +
-          "from gc_cube where id = " +cubeId+  ";"
+          "from gc_cube where id = " + cubeId + ";"
         val rs = statement.executeQuery(sql)
-        val resHashMap = new mutable.HashMap[(Double,Double),(Double,Double,Double,Double)]
+        val resHashMap = new mutable.HashMap[(Double, Double), (Double, Double, Double, Double)]
         //add every measurementName
         while (rs.next) {
-          //          val res = rs.getString(1).split("/").head
-          //          println(res.length)
-          //
-          //          val res1 = res.substring(1,res.length-1)
-          //          res1.foreach(e=>{
-          //            print("-----"+e)
-          //          })
-          //          println(res1)
-          //          val small = "0.0025"
-          //          println(res1==small)
-          //
-          //          println(small)
-          //          println(small.toDouble)
-          resHashMap.put((rs.getString(1).toDouble,rs.getDouble(2)),(rs.getDouble(3),rs.getDouble(4),rs.getDouble(5),rs.getDouble(6)))
+          resHashMap.put((rs.getString(1).toDouble, rs.getDouble(2)), (rs.getDouble(3), rs.getDouble(4), rs.getDouble(5), rs.getDouble(6)))
         }
         resHashMap
       } finally {
