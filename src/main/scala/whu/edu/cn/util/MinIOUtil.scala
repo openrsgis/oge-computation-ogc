ffd33574baa0d7ed90be6fd54c03359c50ca4d97
forDecember <1921567337@qq.com>
2023-09-14 11:56:46 +0800

    update: 添加数据库连接池，重构 postgresqlUtil

--- src/main/scala/whu/edu/cn/util/MinIOUtil.scala
+++ src/main/scala/whu/edu/cn/util/MinIOUtil.scala
@@ -3,21 +3,13 @@ package whu.edu.cn.util
 import io.minio._
 import whu.edu.cn.util.GlobalConstantUtil.{MINIO_ACCESS_KEY, MINIO_ENDPOINT, MINIO_MAX_CONNECTIONS, MINIO_SECRET_KEY}
 
+import scala.collection.mutable.ArrayBuffer
+
 object MinIOUtil {
-  private val connectionPool: Array[MinioClient] = Array.fill(MINIO_MAX_CONNECTIONS)(createMinioClient())
+  private val connectionPool: ArrayBuffer[MinioClient] = ArrayBuffer.fill(MINIO_MAX_CONNECTIONS)(createMinioClient())
 
   private def createMinioClient(): MinioClient = {
     lazy val minioClient: MinioClient = MinioClient.builder()
-      .endpoint(MINIO_ENDPOINT)
-      .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
-      .build()
-    minioClient.setTimeout(10*60*10000, 10*60*10000,10*60*10000)
-    minioClient
-  }
-
-  def getMinioClient: MinioClient = {
-    // 从连接池中获取可用的 MinioClient
-    val minioClient: MinioClient = MinioClient.builder()
       .endpoint(MINIO_ENDPOINT)
       .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
       .build()
@@ -25,12 +17,26 @@ object MinIOUtil {
     minioClient
   }
 
-  def releaseMinioClient(client: MinioClient): Unit = {
-    // 将 MinioClient 放回连接池
+  def getMinioClient: MinioClient = {
     connectionPool.synchronized {
-      val index: Int = connectionPool.indexOf(client)
-      if (index != -1) {
-        connectionPool(index) = null
+      // 为空则新建连接
+      if (connectionPool.isEmpty) {
+        return MinioClient.builder()
+          .endpoint(MINIO_ENDPOINT)
+          .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
+          .build()
+      }
+
+      // 否则从连接池获取
+      return connectionPool.remove(connectionPool.size - 1)
+    }
+  }
+
+  def releaseMinioClient(client: MinioClient): Unit = {
+    // 将使用后的 MinioClient 放回连接池
+    connectionPool.synchronized {
+      if (client != null) {
+        connectionPool.append(client)
       }
     }
   }
