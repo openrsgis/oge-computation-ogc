8a66dea686e06d38d287c4c224df150e3133248b
HUAWEI <2015620030@qq.com>
2024-09-19 01:28:07 +0800

    修改algorithms/MLlib

--- src/main/scala/whu/edu/cn/algorithms/MLlib/Evaluator.scala
+++ src/main/scala/whu/edu/cn/algorithms/MLlib/Evaluator.scala
@@ -140,13 +140,14 @@ object Evaluator {
     evaluator.evaluate(assembledDF)
   }
 
-  def multilabelClassificationEvaluator(spark: SparkSession, labelsCoverage: RDDImage, predictionCoverage: RDDImage, metricName: List[String] = List("f1Measure"), predictionBandIndex: Int = 0): List[Double] = {
+  def multilabelClassificationEvaluator(spark: SparkSession, labelsCoverage: RDDImage, predictionCoverage: RDDImage, metricName: List[String] = List("f1Measure")): List[Double] = {
     val labelsRows: Int = findSpatialKeyMinMax(labelsCoverage)._1 //瓦片行数
     val labelsCols: Int = findSpatialKeyMinMax(labelsCoverage)._2 //瓦片列数
     val predictionRows: Int = findSpatialKeyMinMax(predictionCoverage)._1 //瓦片行数
     val predictionCols: Int = findSpatialKeyMinMax(predictionCoverage)._2 //瓦片列数
     if(labelsRows != predictionRows || labelsCols != predictionCols) throw new IllegalArgumentException("传入两影像尺寸不同！")
     val labelsCoverageBandCount: Int = labelsCoverage._1.first()._2.bandCount
+    val predictionCoverageBandCount: Int = predictionCoverage._1.first()._2.bandCount
     val labelsRdd: RDD[((Int, Int, Int, Int), List[Double])] = labelsCoverage._1.flatMap(t=>{
       val list: ListBuffer[((Int, Int, Int, Int), List[Double])] = ListBuffer.empty[((Int, Int, Int, Int), List[Double])]
       //定位瓦片
@@ -162,39 +163,51 @@ object Evaluator {
       }
       list.toList
     })
-    val predictedRdd: RDD[((Int, Int, Int, Int), Double)] = predictionCoverage._1.flatMap(t=>{
-      val list: ListBuffer[((Int, Int, Int, Int), Double)] = ListBuffer.empty[((Int, Int, Int, Int), Double)]
+    val predictedRdd: RDD[((Int, Int, Int, Int), List[Double])] = predictionCoverage._1.flatMap(t=>{
+      val list: ListBuffer[((Int, Int, Int, Int), List[Double])] = ListBuffer.empty[((Int, Int, Int, Int), List[Double])]
       //定位瓦片
       val row = t._1.spaceTimeKey.spatialKey.row
       val col = t._1.spaceTimeKey.spatialKey.col
       //定位像素
       for(i <- 0 until 256; j <- 0 until 256){  // (i, j)是像素在该瓦片中的定位
-        list.append(((row, col, i, j), t._2.band(predictionBandIndex).getDouble(j, i)))  //使用4个label标识像素在哪个瓦片的哪个位置
+        val predictionList: ListBuffer[Double] = ListBuffer.empty[Double]
+        for(index <- 0 until predictionCoverageBandCount){
+          predictionList.append(t._2.band(index).getDouble(j, i))
+        }
+        list.append(((row, col, i, j), predictionList.toList))  //使用4个label标识像素在哪个瓦片的哪个位置
       }
       list.toList
     })
     //join两影像RDD
-    val joinRdd: RDD[((Int, Int, Int, Int), (List[Double], Double))] = labelsRdd.join(predictedRdd)
+    val joinRdd: RDD[((Int, Int, Int, Int), (List[Double], List[Double]))] = labelsRdd.join(predictedRdd)
     val rowRdd: RDD[Row] = joinRdd.map(t => {
-      val list = List(t._1._1.toDouble, t._1._2.toDouble, t._1._3.toDouble, t._1._4.toDouble) ::: t._2._1 ::: List(t._2._2)
+      val list = List(t._1._1.toDouble, t._1._2.toDouble, t._1._3.toDouble, t._1._4.toDouble) ::: t._2._1 ::: t._2._2
       Row(list:_*)
     })
-    val fieldTypes = List.fill(5+labelsCoverageBandCount)(DoubleType)
+    val fieldTypes = List.fill(4+labelsCoverageBandCount+predictionCoverageBandCount)(DoubleType)
     val colNames: ListBuffer[String] = ListBuffer.empty[String]
     for (i <- 1 to labelsCoverageBandCount) {
       colNames.append(s"labele$i")
     }
-    val locColNames = List("loc1","loc2","loc3","loc4") ::: colNames.toList ::: List("prediction")
+    for (i <- 1 to predictionCoverageBandCount) {
+      colNames.append(s"prediction$i")
+    }
+    val locColNames = List("loc1","loc2","loc3","loc4") ::: colNames.toList
     val fields = locColNames.zip(fieldTypes).map { case (name, dataType) =>
       StructField(name, dataType)
     }
     val schema = StructType(fields)
     val df = spark.createDataFrame(rowRdd, schema).toDF(locColNames:_*)
-    val assembler = new VectorAssembler()
-      .setInputCols(colNames.toArray)
+    val assembler1 = new VectorAssembler()
+      .setInputCols(colNames.slice(0,labelsCoverageBandCount).toArray)
       .setOutputCol("labels")
       .setHandleInvalid("skip")
-    val assembledDF = assembler.transform(df)
+    val assembler2 = new VectorAssembler()
+      .setInputCols(colNames.slice(labelsCoverageBandCount,colNames.length).toArray)
+      .setOutputCol("prediction")
+      .setHandleInvalid("skip")
+    val assembledDF1 = assembler1.transform(df)
+    val assembledDF = assembler2.transform(assembledDF1)
     val multilabelMetricList: List[String] = List("f1Measure", "subsetAccuracy", "accuracy", "hammingLoss", "precision", "recall", "precisionByLabel", "recallByLabel", "f1MeasureByLabel", "microPrecision", "microRecall", "microF1Measure")
     val accuracyList: ListBuffer[Double] = ListBuffer.empty[Double]
     for (name <- metricName){
@@ -210,12 +223,11 @@ object Evaluator {
     accuracyList.toList
   }
 
-  def binaryClassificationEvaluator(spark: SparkSession, labelCoverage: RDDImage, predictionCoverage: RDDImage, metricName: List[String] = List("areaUnderROC"), labelBandIndex: Int = 0): List[Double] = {
+  def binaryClassificationEvaluator(spark: SparkSession, labelCoverage: RDDImage, predictionCoverage: RDDImage, metricName: List[String] = List("areaUnderROC"), labelBandIndex: Int = 0, predictionBandIndex: Int = 0): List[Double] = {
     val labelRows: Int = findSpatialKeyMinMax(labelCoverage)._1 //瓦片行数
     val labelCols: Int = findSpatialKeyMinMax(labelCoverage)._2 //瓦片列数
     val predictionRows: Int = findSpatialKeyMinMax(predictionCoverage)._1 //瓦片行数
     val predictionCols: Int = findSpatialKeyMinMax(predictionCoverage)._2 //瓦片列数
-    val predictionCoverageBandCount = predictionCoverage._1.first()._2.bandCount
     if(labelRows != predictionRows || labelCols != predictionCols) throw new IllegalArgumentException("传入两影像尺寸不同！")
     val labelRdd: RDD[((Int, Int, Int, Int), Double)] = labelCoverage._1.flatMap(t=>{
       val list: ListBuffer[((Int, Int, Int, Int), Double)] = ListBuffer.empty[((Int, Int, Int, Int), Double)]
@@ -228,43 +240,30 @@ object Evaluator {
       }
       list.toList
     })
-    val predictedRdd: RDD[((Int, Int, Int, Int), List[Double])] = predictionCoverage._1.flatMap(t=>{
-      val list: ListBuffer[((Int, Int, Int, Int), List[Double])] = ListBuffer.empty[((Int, Int, Int, Int), List[Double])]
+    val predictedRdd: RDD[((Int, Int, Int, Int), Double)] = predictionCoverage._1.flatMap(t=>{
+      val list: ListBuffer[((Int, Int, Int, Int), Double)] = ListBuffer.empty[((Int, Int, Int, Int), Double)]
       //定位瓦片
       val row = t._1.spaceTimeKey.spatialKey.row
       val col = t._1.spaceTimeKey.spatialKey.col
       //定位像素
       for(i <- 0 until 256; j <- 0 until 256){  // (i, j)是像素在该瓦片中的定位
-        val lalbelsList: ListBuffer[Double] = ListBuffer.empty[Double]
-        for(index <- 0 until predictionCoverageBandCount){
-          lalbelsList.append(t._2.band(index).getDouble(j, i))
-        }
-        list.append(((row, col, i, j), lalbelsList.toList))  //使用4个label标识像素在哪个瓦片的哪个位置
+        list.append(((row, col, i, j), t._2.band(predictionBandIndex).getDouble(j, i)))  //使用4个label标识像素在哪个瓦片的哪个位置
       }
       list.toList
     })
     //join两影像RDD
-    val joinRdd: RDD[((Int, Int, Int, Int), (Double, List[Double]))] = labelRdd.join(predictedRdd)
+    val joinRdd: RDD[((Int, Int, Int, Int), (Double, Double))] = labelRdd.join(predictedRdd)
     val rowRdd: RDD[Row] = joinRdd.map(t => {
-      val list = List(t._1._1.toDouble, t._1._2.toDouble, t._1._3.toDouble, t._1._4.toDouble, t._2._1) ::: List(t._2._2)
+      val list = List(t._1._1.toDouble, t._1._2.toDouble, t._1._3.toDouble, t._1._4.toDouble, t._2._1, t._2._2)
       Row(list:_*)
     })
-    val fieldTypes = List.fill(5+predictionCoverageBandCount)(DoubleType)
-    val colNames: ListBuffer[String] = ListBuffer.empty[String]
-    for (i <- 1 to predictionCoverageBandCount) {
-      colNames.append(s"prediction$i")
-    }
-    val locColNames = List("loc1","loc2","loc3","loc4") ::: List("label") ::: colNames.toList
+    val fieldTypes = List.fill(4+2)(DoubleType)
+    val locColNames = List("loc1","loc2","loc3","loc4") ::: List("label") ::: List("prediction")
     val fields = locColNames.zip(fieldTypes).map { case (name, dataType) =>
       StructField(name, dataType)
     }
     val schema = StructType(fields)
     val df = spark.createDataFrame(rowRdd, schema).toDF(locColNames:_*)
-    val assembler = new VectorAssembler()
-      .setInputCols(colNames.toArray)
-      .setOutputCol("prediction")
-      .setHandleInvalid("skip")
-    val assembledDF = assembler.transform(df)
     val binaryMetricList: List[String] = List("areaUnderROC", "areaUnderPR")
     val accuracyList: ListBuffer[Double] = ListBuffer.empty[Double]
     for (name <- metricName){
@@ -273,7 +272,7 @@ object Evaluator {
           .setLabelCol("label")
           .setRawPredictionCol("prediction")
           .setMetricName(name)
-        accuracyList.append(evaluator.evaluate(assembledDF))
+        accuracyList.append(evaluator.evaluate(df))
       }
       else throw new IllegalArgumentException(s"metricName中包含不支持的精度评估方法$name！")
     }
